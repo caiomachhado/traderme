446b44ebc0d7b8996a030c0e8b2d683f32b759d9
caiomachhado <caioamsul@gmail.com>
2022-05-25 08:02:05 -0300

    Att

--- app/src/main/java/com/example/traderme/AddCourseActivity.kt
+++ app/src/main/java/com/example/traderme/AddCourseActivity.kt
@@ -2,10 +2,14 @@ package com.example.traderme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 
 class AddCourseActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_course)
+
+
     }
 }
